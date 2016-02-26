ec5bdaa82a8effbc250d56997263d171de05b280
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-02-26 12:35:43 -0500

    Add method to generate random playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,10 +1,20 @@
+require 'json'
 class Playlist
   def initialize(search_term)
-    @search_term = HTTParty.get("https://api.spotify.com/v1/search?q=%22#{search_term}%22&type=playlist")
+    @search_term = JSON.parse(File.read("playlist.json"))
+    # @search_term = HTTParty.get("https://api.spotify.com/v1/search?q=%22#{search_term}%22&type=playlist")
   end
 
-  def url
-    #extract url
-    return url
+  def playlist_list
+    playlists = @search_term["playlists"]["items"]
+    playlist_array = []
+    playlists.each do |s|
+      playlist_array << "#{s["external_urls"]["spotify"]}"
+    end
+    playlist_array
+  end
+
+  def random_playlist
+    self.playlist_list.sample
   end
 end
