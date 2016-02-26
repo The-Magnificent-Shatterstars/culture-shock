62d323594fe888c9c29697b39612d315056de25b
LylyG <lgalarza@spoonflower.com>
2016-02-26 16:05:29 -0500

    Adjust controller.

--- app/controllers/recipes_playlists_controller.rb
+++ app/controllers/recipes_playlists_controller.rb
@@ -2,6 +2,9 @@ class RecipesPlaylistsController < ApplicationController
   def show
     playlist = Playlist.new(params["genre"])
     render json: playlist.random_playlist
+
+    recipe = Recipe.new(param["genre"])
+    render json: recipe.random_recipe
   end
 
   def index
