31840bb96640a003eaf8f86bf4f8411ba9a30faf
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-02-28 14:08:12 -0500

    Pre-merge commit

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,8 +1,8 @@
-require 'byebug'
 require 'httparty'
+require 'byebug'
 class Playlist
   def initialize(search_term)
-    @search_term = JSON.parse(File.read("app/models/playlist.json"))
+    @search_term = JSON.parse(File.read("playlist.json"))
     # @search_term = HTTParty.get("https://api.spotify.com/v1/search?q=%22#{search_term}%22&type=playlist")
   end
 
@@ -12,29 +12,27 @@ class Playlist
     # playlists.map{|a| a["external_urls"]["spotify"]}.sample
   end
 
-  # Would this sort of structure work to filter through the "total" for short or long playlists?
   def random_playlist(length)
     get_playlists
     if length == "short"
-      short_playlists = playlists.select {|a| a["total"] < 100 }
+      short_playlists = get_playlists.select {|a| a["tracks"]["total"] < 100 }
       short_playlists.sample
     elsif length == "long"
-      long_playlists = playlists.select {|a| a["total"] > 100 }
+      long_playlists = get_playlists.select {|a| a["tracks"]["total"] > 100 }
       long_playlists.sample
     else
       playlists = @search_term["playlists"]["items"]
       playlists.map{|a| a["external_urls"]["spotify"]}.sample
+      # selection = "You didn't specify a length, so I will select a random playlist for you."
     end
+
   end
 
     # def total
-    #   random_playlist["owner"]["tracks"]["total"]
+    #   selection["tracks"]["total"]
     # end
-
-  # def get_playlist_image
-  # end
 end
 
 
 # newlist = Playlist.new("kpop")
-# puts newlist.get_playlists
+# puts newlist.total
