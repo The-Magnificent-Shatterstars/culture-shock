69dd079496cc7c507675edef4cf1925517478dcc
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-02-28 14:10:02 -0500

    pre-merge commit

--- app/controllers/recipes_playlists_controller.rb
+++ app/controllers/recipes_playlists_controller.rb
@@ -1,8 +1,7 @@
 class RecipesPlaylistsController < ApplicationController
   def show
-     @food = Recipe.new(JSON.parse(File.read("test/fixtures/recipe_tests.json")))
+   @food = Recipe.new(JSON.parse(File.read("test/fixtures/recipe_tests.json")))
    @music = Playlist.new(JSON.parse(File.read("test/fixtures/playlist.json")))
-
    render json: {recipe:
                    {name: "#{@food.title}",
                     recipe_source: "#{@food.source}",
@@ -12,10 +11,7 @@ class RecipesPlaylistsController < ApplicationController
                   }
                 }
               }
-
   end
-
-
   def index
   end
 end
